e3fa559057cdfc4ff228aa1046c678e678868c75
David Schnell-Davis <dschnelldavis@gmail.com>
2018-01-18 14:55:27 -0800

    Update angular2-json-schema-form to version 0.7.0-alpha.1.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'the Angular JSON Schema Form<br> Bootstrap 4 Seed App';
+  title = 'Angular JSON Schema Form Bootstrap 4 Seed App';
 
   exampleSchema = {
     'type': 'object',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { JsonSchemaFormModule } from 'angular2-json-schema-form';
+import { JsonSchemaFormModule, Bootstrap4FrameworkModule } from 'angular2-json-schema-form';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [ AppComponent ],
-  imports: [ BrowserModule, NoopAnimationsModule, JsonSchemaFormModule ],
+  imports: [
+    BrowserModule, Bootstrap4FrameworkModule, JsonSchemaFormModule.forRoot(Bootstrap4FrameworkModule)
+  ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
